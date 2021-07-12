c49643c14313f57ff0319743bea77e6de6680e15
justteen <48372964+justteen@users.noreply.github.com>
2021-07-13 00:37:26 +0700

    Create __init__.py

---
+++ VCPlayBot/services/callsmusic/queues/__init__.py
@@ -0,0 +1,7 @@
+from VCPlayBot.services.callsmusic.queues import clear 
+from VCPlayBot.services.callsmusic.queues import get
+from VCPlayBot.services.callsmusic.queues import is_empty
+from VCPlayBot.services.callsmusic.queues import put
+from VCPlayBot.services.callsmusic.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
